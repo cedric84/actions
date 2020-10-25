68f5025152c9e5035516fd35b20f4274eb73c2f4
cedric <dabrao@gmail.com>
2020-10-25 12:24:51 +0100

    updating main.c

--- main.c
+++ main.c
@@ -4,6 +4,9 @@ int
 main(int argc, char* argv[])
 {
 	printf("%s started\n", __FUNCTION__);
+	for (int i = 0; i < argc; i++) {
+		printf("->%s<-\n", argv[i]);
+	}
 	printf("%s terminated\n", __FUNCTION__);
 	return 0;
 }
