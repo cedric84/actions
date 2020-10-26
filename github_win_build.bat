c6b6894df189cb910ef31570fa96ef5ae868a238
cedric <dabrao@gmail.com>
2020-10-26 03:47:17 +0100

    updating YAML file.

--- github_win_build.bat
+++ github_win_build.bat
@@ -1,4 +1,9 @@
 @echo off
-@REM gcc -Wall -Werror -o%GITHUB_WORKSPACE%\app.exe ./main.c
-@gcc -Wall -Werror -oapp.exe ./main.c
+gcc -Wall -Werror -o%GITHUB_WORKSPACE%\app.exe ./main.c
+REM gcc -Wall -Werror -oapp.exe ./main.c
+
+echo "======================"
+SET
+echo "======================"
+dir %GITHUB_WORKSPACE%
 
