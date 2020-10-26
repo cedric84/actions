bbfa5343214826905e52718011d275cd99029145
cedric <dabrao@gmail.com>
2020-10-26 04:18:46 +0100

    updating windows building script.

--- github_win_build.bat
+++ github_win_build.bat
@@ -1,9 +1,3 @@
 @echo off
-gcc -Wall -Werror -oapp.exe ./main.c
-REM gcc -Wall -Werror -oapp.exe ./main.c
-
-echo "======================"
-SET
-echo "======================"
-dir %GITHUB_WORKSPACE%
+gcc -march=i386 -Wall -Werror -oapp.exe ./main.c
 
